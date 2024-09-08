cc9128d3f6501e1eeaf57a0fc2e2c364be93cf96
Carolina Segura <carosegural13@gmail.com>
2024-09-08 18:32:58 -0500

    features(api_rest): add rest_framework and make serializer

--- backendPhoto/urls.py
+++ backendPhoto/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from myapp import urls as myapp_urls
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
+    path('api/v1/', include(myapp_urls))
 ]   
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import UserSerializer, PostSerializer, InteractionSerializer
+from .models import User, Post, Interaction
 
-# Create your views here.
+# Lista todos los usuarios
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Lista todos los posts
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# Lista todas las interacciones
+class InteractionViewSet(viewsets.ModelViewSet):
+    queryset = Interaction.objects.all()
+    serializer_class = InteractionSerializer
