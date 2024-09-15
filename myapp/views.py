331b28db392658036222a0455e23f0a1e4733dc7
Carolina Segura <carosegural13@gmail.com>
2024-09-15 08:51:57 -0500

    fix(api_rest): Fix api rest rest erros images and add settings for photo media support

--- myapp/views.py
+++ myapp/views.py
@@ -42,23 +42,23 @@ class PostDetail(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
-        serializer = PostSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():      
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)    
         print(serializer.errors)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
-    
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def put(self, request, pk):
         post = Post.objects.get(pk=pk)
-        serializer = Post(post, data=request.data, partial=True)
+        serializer = PostSerializer(post, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        post = post.objects.get(pk=pk)
+        post = Post.objects.get(pk=pk)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
@@ -66,7 +66,7 @@ class InteractionDetail(APIView):
     def get(self, request, pk=None):
         inter = Interaction.objects.all();
         serializer = InteractionSerializer(inter, many=True)
-        return Response()
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
         serializer = InteractionSerializer(data=request.data)
@@ -78,7 +78,7 @@ class InteractionDetail(APIView):
     
     def put(self, request, pk):
         inter = Interaction.objects.get(pk=pk)
-        serializer = Interaction(inter, data=request.data, partial=True)
+        serializer = InteractionSerializer(inter, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
